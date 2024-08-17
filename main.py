c88dc64ba4e1ed9aa052cb890344097280765230
TanisshaDash <tanissha.1610@gmail.com>
2024-08-17 12:42:02 +0530

    GUESS THE NUMBER

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello")
+import random 
+target = random.randint(1,100)
 
+while True:
+    Userchoice= input("GUESS THE NUMBER or QUIT(q)")
+    if(Userchoice == "q"):
+         print("QuIt!!?")
+         break 
+
+    Userchoice = int(Userchoice)
+    if(Userchoice==target):
+        print("CORRECT GUESS!!!YAYY")
+        break
+    elif(Userchoice<target):
+        print("Number too SMALL,try another")
+    elif(Userchoice>target):
+        print("Number too BIG,try another")
+   
+print("--GAMEOVER--\n--WANNA TRY AGAIN?")
+
+
+
+         
